825cc0bb5017ebc5a349a71308773eb6c1f1cf8b
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2025-09-30 12:24:18 -0300

    pipeline CI

---
+++ src/test/kotlin/printscript/authorization_service/AuthorizationServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package printscript.authorization_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
